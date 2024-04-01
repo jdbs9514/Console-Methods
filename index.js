cf908724df98b359617150a8bcbfff0053948302
Josue Benavides <jdbs9514@gmail.com>
2024-04-01 15:55:22 -0500

    added some console methods

--- index.js
+++ index.js
@@ -22,3 +22,54 @@ console.log('%c I am some great text', 'font-size:50px; background:red; text-sha
 console.warn('Oh Noo!!')
 
 // Error :|
+console.error('try again baby!')
+
+// Info
+console.info('Crocodiles eat 3-4 people per year')
+
+// Testing
+console.assert(p.classList.contains('ouch'), 'That is wrong!')
+
+// clearing
+console.clear();
+
+// Grouping together
+dogs.forEach(dog => {
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+  console.groupEnd(`${dog.name}`);
+});
+
+// counting
+
+console.count('Wes');
+console.count('Wes');
+console.count('Steve');
+console.count('Steve');
+console.count('Wes');
+console.count('Steve');
+console.count('Wes');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+
+// timing
+console.time('fetching data');
+fetch('https://api.github.com/users/wesbos')
+  .then(data => data.json())
+  .then(data => {
+    console.timeEnd('fetching data');
+    console.log(data);
+  });
+
+console.table(dogs);
+
+// Viewing DOM Elements
+console.log(p);
+console.dir(p);
+
+// console.clear();
